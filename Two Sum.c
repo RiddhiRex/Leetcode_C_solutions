34e58527a5934537cbbb3007199192a664bba1c6
Riddhi Rex <rantonyrex@cs.stonybrook.edu>
2024-03-16 19:08:14 -0700

    Update Two Sum.c

--- Two Sum.c
+++ Two Sum.c
@@ -1,3 +1,43 @@
+solution 1:
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ struct numbers {
+     int idx;
+     int val;
+ };
+int compare(const void *a, const void *b) {
+    return ((struct numbers *)a)->val - ((struct numbers *)b)->val;
+}
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int i=0, j=0;
+    int *res=(int *)malloc(*returnSize * sizeof(int));
+    struct numbers *lookup = (struct numbers*)malloc(numsSize * sizeof(struct numbers));
+    int end =0;
+    for(i=0;i<numsSize;i++) {
+        for(j=0;j<end;j++) {
+            if((nums[i]+lookup[j].val)==target) {
+                res[0]=i;
+                res[1]=lookup[j].idx;
+                free(lookup);
+                return res;
+            } 
+            
+        }
+        lookup[end].val=nums[i];
+        lookup[end].idx=i;
+        end+=1;
+    }
+    res[0]=-1;
+    res[1]=-1;
+    free(lookup);
+    return res;
+}
+
+
+
+solution 2:
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
